d94d6de85176ffab7125889291d122420e00136c
Hardsicked <hardsicked@gmail.com>
2019-08-23 15:00:54 -0300

    Mais Lixo Removido

--- forms_bkp/cadastro/subgrupos.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include "../../php/connect.php";
-	$sql1 = "SELECT * FROM tbgruposagente";
-	$qry1 = mysqli_query($link,$sql1);
-?>
-	
--- forms_bkp/desativado.php
+++
@@ -1,1 +0,0 @@
-<h3 class="text-center"><img width="50px" height="50px" src="img/icons/alert.png"/>  <b>Desativado para correções</b></h3>
